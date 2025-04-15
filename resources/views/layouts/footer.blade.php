98e9c7532ec40febc25f23c6697411fad961a59d
Rohmad Aditya <rohmadaditya21@gmail.com>
2025-04-15 15:50:42 +0700

    fix : memperbaiki content footer, mengubah kata/kalimat header & footer ke bahasa indonesia

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,58 +1,53 @@
 {{-- <div class="container">
-    <div class="row">
-      <div class="col-lg-12 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.25s">
-        <p>© Copyright 2021 Space Dynamic Co. All Rights Reserved. 
-        
-        <br>Design: <a rel="nofollow" href="https://templatemo.com">TemplateMo</a></p>
-      </div>
-    </div>
-  </div> --}}
+  <div class="row">
+    <div class="col-lg-12 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.25s">
+      <p>© Copyright 2021 Space Dynamic Co. All Rights Reserved.
 
-  <footer class="custom-footer">
-    <div class="footer-content">
-      <div class="footer-col">
-        <h2>Altama</h2>
-        <p>
-          Duis cursus, mi quis viverra ornare, eros dolor interdum nulla utimp 
-          erdiet commodo diam libero vitae nibh et jus cursus id rutrum lore 
-          imperdiet ut sem vitae risus tristique posuere
-        </p>
-      </div>
-  
-      <div class="footer-col">
-        <h3>Company</h3>
-        <ul>
-          <li><a href="#">About Us</a></li>
-          <li><a href="#">Contact Us</a></li>
-          <li><a href="#">Core Services</a></li>
-          <li><a href="#">Our Team</a></li>
-          <li><a href="#">Pricing Plan</a></li>
-        </ul>
-      </div>
-  
-      <div class="footer-col">
-        <h3>Useful Links</h3>
-        <ul>
-          <li><a href="#">Contact Us</a></li>
-          <li><a href="#">Privacy Policy</a></li>
-          <li><a href="#">App Download</a></li>
-          <li><a href="#">How It Works</a></li>
-          <li><a href="#">Terms & Conditions</a></li>
-        </ul>
-      </div>
-  
-      <div class="footer-col">
-        <h3>Get In Touch</h3>
-        <ul class="contact-info">
-          <li><i class="fa fa-phone"></i> +12023459999</li>
-          <li><i class="fa fa-envelope"></i> supportvast@gmail.com</li>
-          <li><i class="fa fa-map-marker"></i> 3004 3 rd Ln, Los Angeles, Calnia, 11 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet dolorem rem maxime unde, autem nesciunt ipsa</li>
-        </ul>
-      </div>
+        <br>Design: <a rel="nofollow" href="https://templatemo.com">TemplateMo</a>
+      </p>
     </div>
-  
-    <div class="footer-bottom">
-      <p>&copy; 2022 All Rights Reserved by <span>altamasoft</span>.</p>
+  </div>
+</div> --}}
+
+<footer class="custom-footer">
+  <div class="footer-content">
+    <div class="footer-col">
+      <h2>Altama</h2>
+      <p>
+        Di Altamasoft, kami tidak hanya membuat website dan aplikasi mobile. 
+        kami membangun solusi digital yang dirancang khusus untuk mendukung pertumbuhan bisnis Anda.
+      </p>
     </div>
-  </footer>
-  
+
+    <div class="footer-col">
+      <h3>Layanan Kami</h3>
+      <ul>
+        <li><a href="#">Website Development</a></li>
+        <li><a href="#">Mobile Development</a></li>
+        {{-- <li><a href="#">Core Services</a></li>
+        <li><a href="#">Our Team</a></li>
+        <li><a href="#">Pricing Plan</a></li> --}}
+      </ul>
+    </div>
+
+    <div class="footer-col">
+      <h3>Kantor Kami</h3>
+      <ul class="contact-info">
+        <li><i class="fa fa-map-marker"></i> 3004 3 rd Ln, Los Angeles, Calnia, 11 Lorem ipsum dolor sit, amet
+          consectetur adipisicing elit. Eveniet dolorem rem maxime unde, autem nesciunt ipsa</li>
+      </ul>
+    </div>
+
+    <div class="footer-col">
+      <h3>Kontak Kami</h3>
+      <ul class="contact-info">
+        <li><i class="fa fa-phone"></i> +62 895123456</li>
+        <li><i class="fa fa-envelope"></i> rohmad@gmail.com</li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="footer-bottom">
+    <p>&copy; 2022 All Rights Reserved by <span>altamasoft</span>.</p>
+  </div>
+</footer>
