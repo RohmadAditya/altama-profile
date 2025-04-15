b09aefd2d51de601c0a5a8bde2a1a9c9530d9297
Rohmad Aditya <rohmadaditya21@gmail.com>
2025-04-15 15:38:41 +0700

    fix : memperbarui warna dasar, dan mengganti beberapa background image about us & contact

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -50,22 +50,22 @@
           <div class="row">
             <div class="col-lg-6 align-self-center">
               <div class="left-content header-text wow fadeInLeft" data-wow-duration="1s" data-wow-delay="1s">
-                <h6>Welcome to Space Dynamic</h6>
-                <h2>We Make <em>Digital Ideas</em> &amp; <span>SEO</span> Marketing</h2>
-                <p>Space Dynamic is a professional looking HTML template using a Bootstrap 5 (beta 2). This CSS template is free for you provided by <a rel="nofollow" href="https://templatemo.com/page/1" target="_parent">TemplateMo</a>.</p>
-                <form id="search" action="#" method="GET">
+                <h6>Selamat Datang di Altamasoft</h6>
+                <h2>Kami Membawa <em>Solusi Digital </em> &amp; <span>Inovasi</span> Teknologi</h2>
+                <p>Altamasoft adalah perusahaan profesional yang bergerak di bidang jasa pembuatan website dan aplikasi mobile.</p>
+                {{-- <form id="search" action="#" method="GET">
                   <fieldset>
                     <input type="address" name="address" class="email" placeholder="Your website URL..." autocomplete="on" required>
                   </fieldset>
                   <fieldset>
                     <button type="submit" class="main-button">Analyze Site</button>
                   </fieldset>
-                </form>
+                </form> --}}
               </div>
             </div>
             <div class="col-lg-6">
               <div class="right-image wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
-                <img src="assets/images/banner-right-image.png" alt="team meeting">
+                <img src="{{ asset('assets/images/logo-altama.png') }}" alt="team meeting">
               </div>
             </div>
           </div>
